c30ce4d2637392cba6f1287bf04255a88b8f5358
Anastasiya Lisovenko <lisovenko.ad@gmail.com>
2016-05-05 18:32:42 +0300

    update d-day page

---
+++ js/main.js
@@ -0,0 +1,18 @@
+(function detectIe() {
+    var ie = 0;
+    try { ie = navigator.userAgent.match( /(MSIE |Trident.*rv[ :])([0-9]+)/ )[ 2 ]; }
+    catch(e){}
+    if (ie !== 0) document.getElementsByTagName("html")[0].className += " ie v" + ie;
+ }());
+
+$(document).ready(function() {
+    $('.scrollTo-link').on('click', function(e){
+		var target=$(this).attr('href');
+
+		if ($(target).length){
+			console.log(target);
+			$('html, body').animate({'scrollTop':$(target).offset().top},600);
+		}
+		e.preventDefault();	
+	})
+ });
