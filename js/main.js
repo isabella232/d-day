970c458df1984f04b3d7332317cb6777d0fcf5e8
Anastasiya Lisovenko <lisovenko.ad@gmail.com>
2017-07-13 13:50:46 +0300

    registration with ajax

---
+++ js/main.js
@@ -0,0 +1,46 @@
+function sendAjax(method, url, data, callbackSucces, callbackError){
+    var xhr = new XMLHttpRequest();
+    xhr.open(method, url, true);    
+    xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');     
+
+    xhr.onreadystatechange = function() {
+      if (this.readyState != 4) return;
+
+      if (this.status != 200) {
+        console.log( 'ошибка: ' + (this.status ? this.statusText : 'запрос не удался') );
+        callbackError(xhr);
+        return;
+      }
+      callback(xhr);
+    }
+
+    xhr.send('param=' + JSON.stringify(data));
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+  var dialog = new mdc.dialog.MDCDialog(document.querySelector('#mdc-dialog-default')),
+      errorDialog = new mdc.dialog.MDCDialog(document.querySelector('#mdc-dialog-error'));
+      
+  document.getElementById("registration").addEventListener("submit", function(e){
+    e.preventDefault();
+
+    var formData = {
+      name: document.getElementById("fname").value,
+      org: document.getElementById("forg").value,
+      email: document.getElementById("femail").value
+    };
+
+    sendAjax('post', 'register.php', formData, 
+      function(xhr){
+        if (xhr.responseText != "0"){
+          dialog.show();
+        } else {
+          errorDialog.show()
+        }
+      },
+      function(xhr){
+        errorDialog.show()
+      }
+    );
+  });
+});
--- register.php
+++ register.php
@@ -1,30 +1,15 @@
 <?php
-$back = 'http://nextgis.ru/d-day';
-if( !isset($_REQUEST['name']) || strlen(trim($_REQUEST['name'])) == 0 || !isset($_REQUEST['org']) || strlen(trim($_REQUEST['org'])) == 0 || !isset($_REQUEST['email']) || strlen(trim($_REQUEST['email'])) == 0) {
-    header("Location: $back");
-    exit();
+
+$param = json_decode($_REQUEST["param"]);
+
+if( !isset($param->name) || strlen(trim($param->name)) == 0 || !isset($param->email) || strlen(trim($param->email)) == 0) {
+    echo "0";
 }
 if( $f = @fopen('d-day.txt', 'a') ) {
-    fwrite($f, trim($_REQUEST['name']).";".trim($_REQUEST['org']).";".trim($_REQUEST['email'])."\n");
+    fwrite($f, trim($param->name).";".trim($param->org).";".trim($param->email)."\n");
     fclose($f);
+    echo "1";
+} else {
+	echo "0";
 }
 ?>
-<!doctype html>
-<html>
-<head>
-<meta charset="utf-8">
-<meta http-equiv="refresh" content="7; url=<?=$back ?>" />
-<title>Регистрация на NextGIS Demo Day 2015</title>
-<link rel="stylesheet" href="style.css">
-</head>
-<body style="margin-top: 200px;">
-<h1>Спасибо за регистрацию!</h1>
-<p>Вы записали: </p>
-<ul>
-<li>Имя: «<?=htmlspecialchars($_REQUEST['name']) ?>»</li>
-<li>Адрес: «<?=htmlspecialchars($_REQUEST['email']) ?>»</li>
-<li>Организация: «<?=htmlspecialchars($_REQUEST['org']) ?>»</li>
-</ul>
-Если хотите записать ещё кого-то, просто отправьте форму ещё раз. Сейчас вернём вас к <a href="<?=$back ?>">странице регистрации</a>.</p>
-</body>
-</html>
